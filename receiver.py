3c09428a89f7e59aa00b9289bddcba3234e381e5
bortok1 <xbortokx@gmail.com>
2022-04-20 15:30:26 +0200

    no more exceprtions!

--- receiver.py
+++ receiver.py
@@ -3,32 +3,8 @@ import serial as ser
 import variables as var
 import checksums as che
 
-#====================Exceptions======================
-class NoAcceptFromSenderException(Exception):
-    pass
 
-
-class ACKException(Exception):
-    pass
-
-
-class NAKException(Exception):
-    pass
-
-
-class InvalidChecksumException(NAKException):
-    pass
-
-
-class InvalidHeaderException(NAKException):
-    pass
-
-
-class WrongPacketNumberException(NAKException):
-    pass
-
-
-#===========================Functions==================
+# ===========================Functions==================
 def receiveMessage(serial_port: ser.Serial, check_sum_type: char):
     message = bytearray()
     for i in range(20):
@@ -36,34 +12,38 @@ def receiveMessage(serial_port: ser.Serial, check_sum_type: char):
         serial_port.write(check_sum_type)
         packet_number = 1
         while True:
-            try:
-                # Sprawdzanie poprawności pakietu
-                block_of_data = checkPacket(serial_port, packet_number, check_sum_type)
-                # Zapisuje kolejne części wiadomości do rozwiązania.
-                message += bytearray(block_of_data)
-                # Zwraca znak ACK do nadawcy.
-                serial_port.write(var.ACK)
-                packet_number += 1
-            except NAKException:
-                # Czyszczenie portu
-                # Jeśli nie odczytany pakiet nie jest prawidłowy, to wyślij NAK,
-                # żeby pakiet został wysłany jeszcze raz.
-                serial_port.read(serial_port.in_waiting)
-                serial_port.write(var.NAK)
-            except ACKException:
-                # Zakończenie transferu i zwrócenie wiadomości.
-                serial_port.write(var.ACK)
-                return bytes(message)
-            except NoAcceptFromSenderException:
-                # Jeżeli transfer nie został zaakceptowany lub skończył się czas nasłuchiwania.
-                raise NoAcceptFromSenderException
-
-    raise NoAcceptFromSenderException
+            # Sprawdzanie poprawności pakietu
+            block_of_data, state = checkPacket(serial_port, packet_number, check_sum_type)
+            # Jeśli wszystko dobrze:
+            match state:
+                case 0:
+                    # Zapisuje kolejne części wiadomości do rozwiązania.
+                    message += bytearray(block_of_data)
+                    # Zwraca znak ACK do nadawcy.
+                    serial_port.write(var.ACK)
+                    packet_number += 1
+                    break
+                case 1:
+                    # Czyszczenie portu.
+                    # Jeśli nie odczytany pakiet nie jest prawidłowy, to wyślij NAK,
+                    # żeby pakiet został wysłany jeszcze raz.
+                    serial_port.read(serial_port.in_waiting)
+                    serial_port.write(var.NAK)
+                    break
+                case 2:
+                    # Zakończenie transferu i zwrócenie wiadomości.
+                    serial_port.write(var.ACK)
+                    return bytes(message)
+                case 3:
+                    # Jeżeli transfer nie został zaakceptowany lub skończył się czas nasłuchiwania.
+                    return bytes("Transfer nie zostal zaakceptowany lub skonczyl sie czas nasluchiwania.", 'ascii')
 
 
 def checkPacket(serial_port: ser.Serial, packet_number: int, check_sum_type: char):
     # sprawdzanie poprawności nagłówka.
-    checkHeader(serial_port, packet_number)
+    if checkHeader(serial_port, packet_number) != 0:
+        return bytearray(), checkHeader(serial_port, packet_number)
+
     # Odczytanie bajtów wiadomości.
     data_block = serial_port.read(128)
 
@@ -73,17 +53,17 @@ def checkPacket(serial_port: ser.Serial, packet_number: int, check_sum_type: cha
     elif check_sum_type == var.C:
         message_checksum = serial_port.read(2)
 
-    # Sprawdzanie czy odczytana checksuma jest równa obliczonej na podstawie bloku wiadomości.
+    # Sprawdzanie, czy odczytana checksuma jest równa obliczonej na podstawie bloku wiadomości.
     calculated_checksum = che.calculateChecksum(data_block, check_sum_type)
 
     if message_checksum != calculated_checksum:
-        raise InvalidChecksumException
+        return data_block, 1
 
-    return data_block
+    return data_block, 0
 
 
 def checkHeader(serial_port: ser.Serial, packet_number: int):
-    # Sprawdzanie czy pierwszy bajt jest równy znakowi SOH.
+    # Sprawdzanie, czy pierwszy bajt jest równy znakowi SOH.
     header = serial_port.read(1)
 
     # Wczytanie drugiego bajtu pakietu
@@ -96,21 +76,21 @@ def checkHeader(serial_port: ser.Serial, packet_number: int):
     completion_number_as_integer = int.from_bytes(completion_number, "big")
 
     if len(header) == 0:
-        raise NoAcceptFromSenderException
+        return 3
     elif header == var.EOT:
-        raise ACKException
+        return 2
     elif header != var.SOH:
-        raise InvalidHeaderException
+        return 1
 
     # Sprawdzanie numeru pakietu.
     if packet_number % 255 != number_as_integer:
-        raise WrongPacketNumberException
+        return 1
 
-    # Sprawdzenie czy dopełnienie jest wysłane prawidłowo.
+    # Sprawdzenie, czy dopełnienie jest wysłane prawidłowo.
     if 255 - (packet_number % 255) != completion_number_as_integer:
-        raise WrongPacketNumberException
+        return 1
 
-    return bytearray(header) + bytearray(block_number) + bytearray(completion_number_as_integer)
+    return 0
 
 
 # Usuwanie znaków SUB z końca wiadomości.
@@ -119,4 +99,3 @@ def removeGarbage(data: bytes):
     while data[-1] == 26:
         del data[-1]
     return data
-
